bab674a2ba15e78550217e7d43574c3b2164bf98
Hez Ronningen <code@hez.ca>
2023-12-09 14:31:24 -0800

    bump elixir and otp ver

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule RpiScreenDimmer.MixProject do
     [
       app: :rpi_screen_dimmer,
       version: @version,
-      elixir: "~> 1.10",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
